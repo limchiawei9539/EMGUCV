ac2a0ed006f1af24d814afc70b1443c4b56606c4
Mike Lim Chia Wei <43591034+limchiawei9539@users.noreply.github.com>
2021-11-02 11:43:47 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace EMGUCV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.snapToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.trainToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.snapToolStripMenuItem,
+            this.trainToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1308, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // snapToolStripMenuItem
+            // 
+            this.snapToolStripMenuItem.Name = "snapToolStripMenuItem";
+            this.snapToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.snapToolStripMenuItem.Text = "Snap";
+            this.snapToolStripMenuItem.Click += new System.EventHandler(this.snapToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(22, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1080, 720);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(1136, 54);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(160, 171);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // trainToolStripMenuItem
+            // 
+            this.trainToolStripMenuItem.Name = "trainToolStripMenuItem";
+            this.trainToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.trainToolStripMenuItem.Text = "Train";
+            this.trainToolStripMenuItem.Click += new System.EventHandler(this.trainToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1308, 505);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ToolStripMenuItem snapToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem trainToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Face;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Collections.Generic;
+using Emgu.CV.Util;
+using Emgu.CV.CvEnum;
+
+namespace EMGUCV
+{
+    public partial class Form1 : Form
+    {
+        VideoCapture capture;
+        static readonly CascadeClassifier cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt.xml");
+        bool is_trained = false;
+        bool is_snap = false;
+        List<Mat> TrainedFaces = new List<Mat>();
+        List<int> PersonLabel = new List<int>();
+        EigenFaceRecognizer recognizer;
+        Mat m=new Mat();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void VideoCapture_ImageGrabbed(object sender, EventArgs e)
+        {
+            try
+            {
+                capture.Retrieve(m);
+                var image = m.ToBitmap();
+
+                //Face Detection
+                Rectangle[] faces = cascadeClassifier.DetectMultiScale(m, 1.1, 4);
+                foreach (var r in faces)
+                {
+                    using (Graphics graphics = Graphics.FromImage(image))
+                    {
+                        using (Pen pen = new Pen(Color.Red, 1))
+                        {
+                            graphics.DrawRectangle(pen, r);
+                            //CvInvoke.PutText(m,"Face",new Point(10,r.Height-10),Emgu.CV.CvEnum.FontFace.HersheySimplex,1.0,new Bgr(Color.Red).MCvScalar);
+                        }
+                        pictureBox1.Image = image;
+                    }
+
+                    //Face Recognition
+                    Image<Bgr, byte> resultImage = m.ToImage<Bgr, byte>();
+                    resultImage.ROI = r;
+                    pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                    if (pictureBox2.Image == null)
+                    {
+                        pictureBox2.Image = resultImage.ToBitmap();
+                    }
+
+                    //Snap Picture
+                    if (is_snap)
+                    {
+                        var path = Directory.GetCurrentDirectory() + @"\Mike";
+                        if (!Directory.Exists(path))
+                        {
+                            Directory.CreateDirectory(path);
+                        }
+                        for (int i = 0; i < 50; i++)
+                        {
+                            resultImage.Resize(200, 200, Emgu.CV.CvEnum.Inter.Cubic).Save(path + @"\Mike_" + i +".jpg");
+                        }
+                        MessageBox.Show("Snap Finished!");
+                        is_snap = false;
+                    }
+
+                    if(is_trained)
+                    {
+                        Image<Gray, byte> grayFaceResult = resultImage.Convert<Gray,byte>().Resize(200,200,Emgu.CV.CvEnum.Inter.Cubic);
+                        var result = recognizer.Predict(grayFaceResult);
+
+                        CvInvoke.PutText(m, "Mike", new Point(r.X - 2, r.Y - 2),
+                            FontFace.HersheyComplex, 1.0, new Bgr(Color.Orange).MCvScalar);
+                        CvInvoke.Rectangle(m, r, new Bgr(Color.Green).MCvScalar, 2);
+                        pictureBox1.Image = m.ToBitmap();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                capture.Start();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (capture != null)
+            {
+                capture.Pause();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (capture == null)
+            {
+                capture = new Emgu.CV.VideoCapture();
+            }
+            capture.Set(Emgu.CV.CvEnum.CapProp.FrameHeight, 1080);
+            capture.Set(Emgu.CV.CvEnum.CapProp.FrameWidth, 1920);
+            capture.ImageGrabbed += VideoCapture_ImageGrabbed;
+            capture.Start();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void snapToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            is_snap = true;
+        }
+
+        private void trainToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            int threshold = 7000;
+            try
+            {
+                var path = Directory.GetCurrentDirectory() + @"\Mike";
+                var files = Directory.GetFiles(path, "*.jpg",SearchOption.AllDirectories);
+                foreach(var f in files)
+                {
+                    Image<Gray, byte> trainedImage = new Image<Gray, byte>(f);
+                    TrainedFaces.Add(trainedImage.Mat);
+                    PersonLabel.Add(count);
+                    count++;
+                }
+                recognizer = new EigenFaceRecognizer(count,threshold);
+                recognizer.Train( new VectorOfMat(TrainedFaces.ToArray()), new VectorOfInt(PersonLabel.ToArray()));
+                is_trained = true;
+                MessageBox.Show("Model Trained!");
+            }
+            catch (Exception ex)
+            {
+                is_trained = false;
+                throw;
+            }
+        }
+    }
+}
